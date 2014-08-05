f4bf3b9539f2337247d02a50ded834fd307dac03
Jannes <jannes.meyer@gmail.com>
2014-08-05 10:54:46 +0200

    Require styles and add hot react component replacement

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+require('main.css'); // webpack
 var Workbench = require('./components/workbench');
 var stores = require('./stores');
 var actions = require('./actions');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 'use strict';
-
 var webpack = require('webpack');
 var path = require('path');
-
 var getPath = path.join.bind(path, __dirname);
+
+var paths = {
+	main: getPath(),
+	modules: getPath('node_modules'),
+	javascripts: getPath('public', 'javascripts'),
+	stylesheets: getPath('public', 'stylesheets')
+};
+
+function BellOnErrorPlugin() {
+}
+BellOnErrorPlugin.prototype.apply = function(compiler) {
+	compiler.plugin('done', function(stats) {
+		console.log('done');
+		// if (stats.hasErrors()) {
+		// 	// System bell character
+		// 	console.log('Error\u0007');
+		// }
+	});
+};
+
 var config = {
 	cache: true,
 	entry: [
@@ -11,39 +29,43 @@ var config = {
 		getPath('src', 'main.js'),
 	],
 	output: {
-		path: getPath('public', 'javascripts'),
+		path: paths.javascripts,
 		filename: '[name].bundle.js',
 		publicPath: '/javascripts/'
 	},
+	watchDelay: 50,
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.ProvidePlugin({
 			'React': 'react',
 			'Fluxxor': 'fluxxor',
 			'Immutable': 'immutable'
-		})
+		}),
+		new BellOnErrorPlugin()
 	],
 	module: {
 		loaders: [
 			{
 				test: /\.jsx$/,
-				loaders: [
-					'react-hot',
-					'jsx?harmony&insertPragma=React.DOM'
-				],
-				include: [ getPath() ],
-				exclude: [ getPath('node_modules') ]
+				loaders: ['react-hot-loader', 'jsx-loader?harmony&insertPragma=React.DOM'],
+				include: [ paths.main ],
+				exclude: [ paths.modules ]
 			},
 			{
 				test: /\.js$/,
-				loader: 'jsx?harmony',
-				include: [ getPath() ],
-				exclude: [ getPath('node_modules') ]
+				loader: 'jsx-loader?harmony',
+				include: [ paths.main ],
+				exclude: [ paths.modules ]
+			},
+			{
+				test: /\.css$/,
+				loaders: ['style-loader', 'css-loader']
 			}
 		]
 	},
 	resolve: {
-		extensions: ['', '.js', '.jsx']
+		root: paths.stylesheets,
+		extensions: ['', '.js', '.jsx', '.json']
 	}
 };
 
