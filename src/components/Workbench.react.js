dc66fdae4d186cbff96aee6543a41a55f6fd18b2
Jannes <jannes.meyer@gmail.com>
2014-08-12 00:22:55 +0200

    Improve style

--- src/components/Workbench.react.js
+++ src/components/Workbench.react.js
@@ -8,66 +8,63 @@ function getState() {
 	};
 }
 
+var drag = null;
+var zCounter = 0;
+
+function getDragPosition(mouseX, mouseY) {
+	var x = drag.mouseX + mouseX;
+	var y = drag.mouseY + mouseY;
+	// Snap to grid:
+	// x = Math.round(x / 5 + 1) * 5 - 1;
+	// y = Math.round(y / 5 + 1) * 5 - 1;
+	return {x, y};
+}
+
 var Workbench = React.createClass({
-	drag: null,
-	zCounter: 0,
 	getInitialState: getState,
-	onChange() {
+	handleChange() {
 		this.setState(getState());
 	},
-	componentDidMount() {
-		FilterStore.addChangeListener(this.onChange);
-	},
-	componentWillUnmount() {
-		FilterStore.removeChangeListener(this.onChange);
-	},
 	handleMouseDown(id, ev) {
-		if (ev.button !== 0) {
-			return;
-		}
+		if (ev.button !== 0) { return; }
+
 		var filter = FilterStore.get(id);
-		this.drag = {
+		drag = {
 			id,
 			element: ev.currentTarget,
-			x: filter.get('x') - ev.clientX,
-			y: filter.get('y') - ev.clientY
+			mouseX: filter.get('x') - ev.clientX,
+			mouseY: filter.get('y') - ev.clientY
 		};
-		this.drag.element.style.zIndex = ++this.zCounter;
+		drag.element.style.zIndex = ++zCounter;
 		ev.preventDefault();
 	},
 	handleMouseMove(ev) {
-		if (!this.drag) {
-			return;
-		}
-		var x = this.drag.x + ev.clientX;
-		var y = this.drag.y + ev.clientY;
-		// Snap to grid
-		// x = Math.round(x / 5 + 1) * 5 - 1;
-		// y = Math.round(y / 5 + 1) * 5 - 1;
-		// Set transform
-		this.drag.element.style.transform = 'translate(' + x + 'px,' + y + 'px)';
+		if (!drag) { return; }
+
+		var {x, y} = getDragPosition(ev.clientX, ev.clientY);
+		drag.element.style.transform = 'translate(' + x + 'px,' + y + 'px)';
 	},
 	handleMouseUp(ev) {
-		if (ev.button !== 0 || !this.drag) {
-			return;
-		}
-		var x = this.drag.x + ev.clientX;
-		var y = this.drag.y + ev.clientY;
-		// Snap to grid
-		// x = Math.round(x / 5 + 1) * 5 - 1;
-		// y = Math.round(y / 5 + 1) * 5 - 1;
-		FilterActions.move(this.drag.id, x, y);
-		this.drag = null;
+		if (!drag || ev.button !== 0) { return; }
+
+		var {x, y} = getDragPosition(ev.clientX, ev.clientY);
+		FilterActions.move(drag.id, x, y);
+		drag = null;
+	},
+	componentDidMount() {
+		FilterStore.addChangeListener(this.handleChange);
+	},
+	componentWillUnmount() {
+		FilterStore.removeChangeListener(this.handleChange);
 	},
 	render() {
 		return (
 			<div className="m-workbench" onMouseMove={this.handleMouseMove} onMouseUp={this.handleMouseUp}>
-			{FilterStore.getAll().map((filter, key) => {
-				return <Filter key={key} onMouseDown={this.handleMouseDown.bind(null, key)} />;
-			}).toArray()}
+			{FilterStore.getAll().map((filter, key) =>
+				<Filter key={key} onMouseDown={this.handleMouseDown.bind(null, key)} />
+			).toArray()}
 			</div>
 		);
 	}
 });
-
 module.exports = Workbench;
