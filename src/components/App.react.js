44b7f0dd97fd9ca56d5e7e949580b8cafe9f997a
Jannes <jannes.meyer@gmail.com>
2014-09-17 11:59:47 +0200

    Move event handler registration to App#componentDidMount

--- src/components/App.react.js
+++ src/components/App.react.js
@@ -13,50 +13,52 @@ function confirmPageUnload(ev) {
 	return message;
 }
 
-// Attach event listeners for global mouse events. It's more accurate
-// to do it on the `window` object rather than on a DOM node like React is doing.
-if (addEventListener) {
-	addEventListener('mousemove', ev => {
-		if (EtherMovementStore.isDragging()) {
-			return AppActions.moveSelectedItems(ev.clientX, ev.clientY);
-		}
-		if (SelectionStore.isSelecting()) {
-			return AppActions.resizeSelection(ev.clientX, ev.clientY);
-		}
-	});
+function handleMouseMove(ev) {
+	if (EtherMovementStore.isDragging()) {
+		return AppActions.moveSelectedItems(ev.clientX, ev.clientY);
+	}
+	if (SelectionStore.isSelecting()) {
+		return AppActions.resizeSelection(ev.clientX, ev.clientY);
+	}
+}
 
-	addEventListener('mouseup', ev => {
-		if (ev.button !== 0) {
-			return;
-		}
-		if (EtherMovementStore.isDragging()) {
-			AppActions.finishMovingSelectedItems(ev);
-			return;
-		}
-		if (SelectionStore.isSelecting()) {
-			AppActions.finishSelection(ev);
-			return;
-		}
-	});
-
-	// TODO: left, right, up, down
-	// Register global keyboard shortcuts
-	keypress.on('backspace',           AppActions.deleteSelectedItems);
-	keypress.on('del',                 AppActions.deleteSelectedItems);
-	keypress.on('z', ['ctrl'],         AppActions.undo);
-	keypress.on('z', ['ctrl','shift'], AppActions.redo);
-	keypress.on('y', ['ctrl'],         AppActions.redo);
-	keypress.on('a', ['ctrl'],         AppActions.selectAll);
-	keypress.on('s', ['ctrl'],         AppActions.exportFile);
-	keypress.on('o', ['ctrl'],         AppActions.importFile);
-
-	// TODO: save architecture in localstorage
-	// addEventListener('beforeunload', confirmPageUnload);
-	// TODO: check for unsaved changes
-	// TODO: removeEventListener('beforeunload', confirmPageUnload);
+function handleMouseUp(ev) {
+	if (ev.button !== 0) {
+		return;
+	}
+	if (EtherMovementStore.isDragging()) {
+		AppActions.finishMovingSelectedItems(ev);
+		return;
+	}
+	if (SelectionStore.isSelecting()) {
+		AppActions.finishSelection(ev);
+		return;
+	}
 }
 
 var App = React.createClass({
+	componentDidMount() {
+		// Register global event handlers
+		// It's more accurate to handle mouse events on the `window` object rather than
+		// on a DOM node like React is doing.
+		addEventListener('mousemove', handleMouseMove);
+		addEventListener('mouseup',   handleMouseUp);
+		keypress.on('backspace',           AppActions.deleteSelectedItems);
+		keypress.on('del',                 AppActions.deleteSelectedItems);
+		keypress.on('z', ['ctrl'],         AppActions.undo);
+		keypress.on('z', ['ctrl','shift'], AppActions.redo);
+		keypress.on('y', ['ctrl'],         AppActions.redo);
+		keypress.on('a', ['ctrl'],         AppActions.selectAll);
+		keypress.on('s', ['ctrl'],         AppActions.exportFile);
+		// TODO: what about the file picker?
+		keypress.on('o', ['ctrl'],         AppActions.importFile);
+		// TODO: left, right, up, down
+
+		// TODO: save architecture in localstorage
+		// addEventListener('beforeunload', confirmPageUnload);
+		// TODO: check for unsaved changes
+		// TODO: removeEventListener('beforeunload', confirmPageUnload);
+	},
 	render() {
 		console.log('App: render');
 		return (
