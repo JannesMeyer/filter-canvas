6bd87131d266cde79171ed694b575bad51889516
Jannes <jannes.meyer@gmail.com>
2015-05-19 21:42:41 +0200

    Fix upload script

--- upload.sh
+++ upload.sh
@@ -3,8 +3,7 @@
 # http://www.damian.oquanta.info/posts/one-line-deployment-of-your-site-to-gh-pages.html
 # git checkout -b gh-pages
 # git rm -rf .
-# git add build
-# git commit -m "Recompile $(date +'%d %b %H:%M')"
+# git commit -m "Initial gh-pages commit"
 # git push origin gh-pages
 # git push origin $(git subtree split --prefix build gh-pages):gh-pages --force
 
